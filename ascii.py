e54789a16a3c7f2c61d1931b75ff9f4d674d25c2
m4xiz34 <maxisacroissant@gmail.com>
2025-12-12 20:36:01 +1100

    Create ascii.py

---
+++ ascii.py
@@ -0,0 +1,8 @@
+A_value = ord("A")
+print("The ASCII value for A is", A_value)
+
+a_value_lowercase = ord("a")
+print("The ASCII value for a is", a_value_lowercase)
+
+c_value = ord("c")
+print("The ASCII value for c is", c_value)
